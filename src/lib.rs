9da7d0387966be705b7dd9f052e4f63ec09dec97
HalArticode <halarticode@aiursoft.tech>
2025-01-30 11:38:35 +0000

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,7 @@ pub use cache::{
 };
 pub use services::{
     dynamic_vector_manage_service::*,
-    // object_persist_on_disk_service::*,
     static_vector_manage_service::*,
 };
 pub use vector_engine::VectorEngine;
 
-// pub use dynamic_vector::{
-    // CheckDynamicSize,
-    // DynamicVector,
-    // VectorCandidate,
-// };
